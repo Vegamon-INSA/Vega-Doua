d7390b7d4b0cd6f682daff541f70515d39d18a7d
Victor Chevillotte <victor.chevillotte@gmail.com>
2020-05-05 21:32:46 +0200

    update chgmt arene

--- FenetreCombat.java
+++ FenetreCombat.java
@@ -171,7 +171,7 @@ public class FenetreCombat implements ActionListener{
 		
         AuTourDuJoueur=false;
         perso.PV=perso.PVmax;      //On réinitialise nos points de vie au max
-        int exp=variablesSession.xpMeloche;
+        int exp=1;
                  // On indique le niveau de l'adversaire en fonction des cas où on est (avec un peu de hasard)
         if (variablesSession.numeroCarte==105 || variablesSession.numeroCarte==301){      //travée1
 			exp=1+(int)(3*Math.random());                    // En fonction de la carte
